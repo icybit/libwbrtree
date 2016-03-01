2d2dd00a4c77712a1aacc94a4f59321122186724
Giedrius Sirevicius <giedriussir@gmail.com>
2016-03-01 18:19:03 +0200

    Fixed incorrect dynamic array macro usage. Updated data types

---
+++ include/common.h
@@ -0,0 +1,9 @@
+#ifndef _RTREE_COMMON_H_
+#define _RTREE_COMMON_H_
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define NELEMS(x) (sizeof(x)/sizeof((x)[0]))
+#define NALLOC(m, M, f) MIN((int)ceil(m * f), M)
+
+#endif
---
+++ include/rectangle.h
@@ -0,0 +1,26 @@
+#ifndef _RTREE_RECTANGLE_H_
+#define _RTREE_RECTANGLE_H_
+
+#include <stdint.h>
+
+struct Point {
+	uint8_t dim;
+	float *coords;
+};
+
+struct Rectangle {
+	uint8_t dim;
+	struct Point *low;
+	struct Point *high;
+};
+
+void point_create(struct Point *dest, uint8_t dimension, float *coordinates);
+
+double rectangle_area(struct Rectangle *rectangle);
+void rectangle_combine(struct Rectangle *rectangle, struct Rectangle *other);
+void rectangle_create(struct Rectangle *dest, struct Point *low, struct Point *high);
+void rectangle_extend_infinitely(struct Rectangle *dest);
+double rectangle_min_distance(struct Rectangle *rectangle, struct Rectangle *other);
+int rectangle_overlaps(struct Rectangle *rectangle, struct Rectangle *other);
+
+#endif
